1596d0caa416b636943dca6f8951928c4c9f924d
Fernando Soto <fersoto117@gmail.com>
2021-12-02 22:44:43 -0800

    3rd day, Pt 1

---
+++ day-03/first-part.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Rate {
+  int gamma;
+  int epsilon;
+} Rate;
+
+Rate calculateRate(int *common_bit, int length) {
+  int gamma = 0;
+  int epsilon = 0;
+  for (int i = 0; i < length; i++) {
+    if (common_bit[i] >= 0) {
+      gamma = gamma | (1 << length - i - 1);
+    } else {
+      epsilon = epsilon | (1 << length - i - 1);
+    }
+  }
+  return (Rate) {gamma, epsilon};
+}
+
+int powerConsumption(Rate rate) {
+  return rate.gamma * rate.epsilon;
+}
+
+int main() {
+  char *binary = malloc(sizeof(char) * 15);
+  int binary_length = 0;
+  int *common_bit;
+
+  for (int i = 0; scanf("%s", binary) != EOF; i++) {
+    if (!i) {
+      binary_length = strlen(binary);
+      common_bit = calloc(binary_length, sizeof(int));
+    }
+    for (int j = 0; j < binary_length; j++) {
+      if (binary[j] == '1') {
+        common_bit[j] ++;
+      } else {
+        common_bit[j] --;
+      }
+    }
+  }
+  Rate rate = calculateRate(common_bit, binary_length);
+  printf("%d", powerConsumption(rate));
+
+  return 0;
+}
