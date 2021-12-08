57071d3c8fad6ff9209faf00d1a16ec0bf1f1062
Fernando Soto <fersoto117@gmail.com>
2021-12-07 21:22:54 -0800

    8th day, pt 1

---
+++ day-08/first-part.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> parseInput(string *input) {
+  size_t pos = 0;
+  vector<string> segments;
+  while ((pos = input->find(" ")) != string::npos) {
+    segments.push_back(stoi(input->substr(0, pos), nullptr));
+    input->erase(0, pos + 1);
+  }
+  segments.push_back(stoi((*input), nullptr));
+  return segments;
+}
+
+int main() {
+    string line;
+    int unique_segments_count = 0;
+    while (getline(cin, line)) {
+        auto segments = parseInput(&line);
+        for (int i = 11; i < segments.size(); i++) {
+            unique_segments_count += (segments[i].length() < 5 || segments[i].lenght() > 6);
+        }
+    }
+    cout << unique_segments_count;
+
+    return 0;
+}
